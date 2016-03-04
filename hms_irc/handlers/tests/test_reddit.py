53bbb8e3ff77845e9350a2e6435a3e99da4d8fb0
Romain Porte <microjoe@mailoo.org>
2016-03-04 16:59:34 +0100

    Added first unit test

---
+++ hms_irc/handlers/tests/test_reddit.py
@@ -0,0 +1,21 @@
+import unittest
+
+from hms_irc.handlers import reddit
+
+
+class TestReddit(unittest.TestCase):
+
+    """Test case for Reddit IRC handler."""
+
+    def test_privmsg_contains_basic_information(self):
+        msg = {
+            'author': 'MicroJoe',
+            'title': 'This is a test',
+            'url': 'http://haum.org'
+        }
+
+        privmsg = reddit.msg_to_privmsg(msg)
+
+        self.assertTrue(msg['author'] in privmsg)
+        self.assertTrue(msg['title'] in privmsg)
+        self.assertTrue(msg['url'] in privmsg)
