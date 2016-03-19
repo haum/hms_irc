ae5975bfd4d7336704da39e6cc61cc0193d0e9e3
Romain Porte <microjoe@mailoo.org>
2016-03-19 18:22:18 +0100

    Added more tests for irc_debug handler

--- hms_irc/handlers/tests/test_irc_debug.py
+++ hms_irc/handlers/tests/test_irc_debug.py
@@ -1,10 +1,25 @@
 import unittest
+from unittest.mock import Mock
 
 from hms_irc.handlers import irc_debug
 
 
 class TestIRCDebug(unittest.TestCase):
 
-    def test_privmsg(self):
-        body = {'privmsg': 'This is a test.'}
-        self.assertEqual(body['privmsg'], irc_debug.msg_to_privmsg(body))
+    def setUp(self):
+        self.body = {'privmsg': 'This is a test.'}
+
+    def test_decode_privmsg(self):
+        self.assertEqual(
+            self.body['privmsg'],
+            irc_debug.msg_to_privmsg(self.body))
+
+    def test_call_privmsg(self):
+        irc_chan = '#haum-test'
+
+        irc_server = Mock()
+        irc_server.privmsg = Mock()
+
+        irc_debug.handle(irc_server, irc_chan, self.body)
+
+        irc_server.privmsg.assert_called_with(irc_chan, self.body['privmsg'])
