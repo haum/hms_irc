60ae4a991d2ae520832d70e8bf9b99d786cd1dbd
Romain Porte <microjoe@mailoo.org>
2016-04-14 17:59:36 +0200

    Factorized tests

--- hms_irc/handlers/tests/test_irc_debug.py
+++ hms_irc/handlers/tests/test_irc_debug.py
@@ -8,6 +8,9 @@ class TestIRCDebug(unittest.TestCase):
 
     def setUp(self):
         self.body = {'privmsg': 'This is a test.'}
+        self.irc_server = Mock()
+        self.irc_server.privmsg = Mock()
+        self.irc_chan = '#haum-test'
 
     def test_decode_privmsg(self):
         self.assertEqual(
@@ -18,21 +21,12 @@ class TestIRCDebug(unittest.TestCase):
         self.assertIsNone(irc_debug.msg_to_privmsg({}))
 
     def test_call_privmsg(self):
-        irc_chan = '#haum-test'
+        irc_debug.handle(self.irc_server, self.irc_chan, self.body)
 
-        irc_server = Mock()
-        irc_server.privmsg = Mock()
-
-        irc_debug.handle(irc_server, irc_chan, self.body)
-
-        irc_server.privmsg.assert_called_once_with(
-            irc_chan, self.body['privmsg'])
+        self.irc_server.privmsg.assert_called_once_with(
+            self.irc_chan, self.body['privmsg'])
 
     def test_call_empty_privmsg(self):
-        irc_chan = '#haum-test'
+        irc_debug.handle(self.irc_server, self.irc_chan, {})
 
-        irc_server = Mock()
-        irc_server.privmsg = Mock()
-
-        irc_debug.handle(irc_server, irc_chan, {})
-        irc_server.privmsg.assert_not_called()
+        self.irc_server.privmsg.assert_not_called()
