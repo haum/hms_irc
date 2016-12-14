76f259cf6c4b2d0e7831fd531f32d6935d27b650
Romain Porte <microjoe@microjoe.org>
2016-12-14 22:15:23 +0100

    Handle SpaceAPI

--- hms_irc/receptors/spacestatus/answer.py
+++ hms_irc/receptors/spacestatus/answer.py
@@ -6,19 +6,36 @@ def get_logger():
     return logging.getLogger(__name__)
 
 
-def dct_to_privmsg(dct):
+def dct_to_privmsgs(dct):
     is_open = dct['is_open']
-
     has_changed = dct['has_changed'] if 'has_changed' in dct else True
 
-    msg = strings.SPACESTATUS_OPEN if is_open else strings.SPACESTATUS_CLOSED
+    # Space status info (open or closed)
+    line1 = strings.SPACESTATUS_OPEN if is_open else strings.SPACESTATUS_CLOSED
 
     # Check if state changed or not
     if not has_changed:
-        msg += strings.SPACESTATUS_SAME_STATE
+        line1 += strings.SPACESTATUS_SAME_STATE
 
-    return msg
+    # On line 2 we want to know SpaceApi response
+    spaceapi_open = dct['spaceapi']['is_open']
+    spaceapi_badssl = dct['spaceapi']['ssl_error']
+    spaceapi_badhttp = dct['spaceapi']['bad_http_code']
+    spaceapi_globalerror = dct['spaceapi']['global_error']
+
+    line2 = '[SpaceAPI] '
+    line2 += strings.SPACEAPI_OPEN if spaceapi_open else strings.SPACEAPI_CLOSED
+
+    if spaceapi_badssl:
+        line2 += ' ({})'.format(strings.SPACEAPI_BAD_SSL)
+    if spaceapi_badhttp:
+        line2 += ' ({})'.format(strings.SPACEAPI_BAD_HTTP_CODE)
+    if spaceapi_globalerror:
+        line2 += ' ({})'.format(strings.SPACEAPI_GLOBAL_ERROR)
+
+    return (line1, line2)
 
 
 def handle(irc_server, irc_chan, dct):
-    irc_server.privmsg(irc_chan, dct_to_privmsg(dct))
+    for msg in dct_to_privmsgs(dct):
+        irc_server.privmsg(irc_chan, msg)
