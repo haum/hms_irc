194ebed62956aa128d0883a3ce2e74961b7e64cc
Romain Porte <microjoe@microjoe.org>
2017-04-28 11:47:44 +0200

    Refactored spacestatus command to IRCHandler

--- hms_irc/commands/spacestatus.py
+++ hms_irc/commands/spacestatus.py
@@ -1,49 +1,51 @@
 from hms_irc import strings
+from hms_irc.irc import IRCHandler
+
 
 VOICED_ARGUMENTS = ['open', 'close', 'open_silent', 'close_silent']
 VALID_ARGUMENTS = VOICED_ARGUMENTS + ['help']
 
 
-def handle(irc_server, irc_chan, rabbit, command):
+class SpaceStatusHandler(IRCHandler):
 
-    # Define some useful closures
+    def handle(self, command):
+        # If no argument provided, just check current status
+        if not command.command_args:
+            self.query('status')
+        else:
+            # Check argument user provided
+            argument = command.command_args[0]
+            if argument not in VALID_ARGUMENTS:
+                self.chanmsg(strings.UNKNOWN_ARGUMENT)
+                self.display_help()
+                return
 
-    def query(command):
-        """Closure for sending a query to the spacestatus microservice."""
-        rabbit.publish(
+            # Check if command needs to be voiced
+            if argument in VOICED_ARGUMENTS and not command.is_voiced:
+                self.chanmsg(strings.MUST_BE_VOICED)
+                return
+
+            # If help required just display it and do not send query
+            if argument == 'help':
+                self.display_help()
+                return
+
+            # The argument is correct, send it to the spacestatus microservice
+            self.query(argument)
+
+    def query(self, command):
+        """Sends query to the spacestatus microservice."""
+        self.rabbit.publish(
             'spacestatus.query', {
                 'command': command,
                 'source': 'irc'
             })
 
-    def chanmsg(msg):
-        """Closure for sending a privmsg on the chan."""
-        irc_server.privmsg(irc_chan, msg)
+    def display_help(self):
+        """Shows help to the user in the chan."""
+        self.chanmsg(strings.SPACESTATUS_HELP.format(VALID_ARGUMENTS))
 
-    def help():
-        """Closure for displaying help."""
-        chanmsg(strings.SPACESTATUS_HELP.format(VALID_ARGUMENTS))
 
-    # If no argument provided, just check current status
-    if not command.command_args:
-        query('status')
-    else:
-        # Check argument user provided
-        argument = command.command_args[0]
-        if argument not in VALID_ARGUMENTS:
-            chanmsg(strings.UNKNOWN_ARGUMENT)
-            help()
-            return
-
-        # Check if command needs to be voiced
-        if argument in VOICED_ARGUMENTS and not command.is_voiced:
-            chanmsg(strings.MUST_BE_VOICED)
-            return
-
-        # If help required just display it and do not send query
-        if argument == 'help':
-            help()
-            return
-
-        # The argument is correct, send it to the spacestatus microservice
-        query(argument)
+def handle(irc_server, irc_chan, rabbit, command):
+    h = SpaceStatusHandler(irc_server, irc_chan, rabbit)
+    h.handle(command)
