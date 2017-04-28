f55636f0c34109d45ebc2efc65d5f998aa4cf5ff
Romain Porte <microjoe@microjoe.org>
2017-04-28 12:19:10 +0200

    Using IRCHandler for toot commands

--- hms_irc/commands/toot.py
+++ hms_irc/commands/toot.py
@@ -1,34 +1,33 @@
 import logging
 
 from hms_irc import strings
-
+from hms_irc.irc import IRCHandler
 
 def get_logger():
     return logging.getLogger(__name__)
 
 
-def handle(irc_server, irc_chan, rabbit, command):
+class TootHandler(IRCHandler):
+    def handle(self, command):
+        get_logger().info('Toot with command {}'.format(command))
 
-    # Define some useful closures
+        # TODO: Ugly way of testing command arguments...
+        if command.command_args and command.command_args[0]:
+            if command.is_voiced:
+                self.toot(' '.join(command.command_args))
+                self.chanmsg(strings.TOOT_PENDING)
+        else:
+            self.chanmsg(strings.TOOT_USAGE)
 
-    def toot(message):
+    def toot(self, msg):
         """Closure for sending a toot."""
-        rabbit.publish(
+        self.rabbit.publish(
             'mastodon.toot', {
-                'message': message,
+                'message': msg,
                 'source': 'irc'
             })
 
-    def chanmsg(msg):
-        """Closure for sending a privmsg on the chan."""
-        irc_server.privmsg(irc_chan, msg)
 
-    get_logger().info('Toot with command {}'.format(command))
-
-    # TODO: Ugly way of testing command arguments...
-    if command.command_args and command.command_args[0]:
-        if command.is_voiced:
-            toot(' '.join(command.command_args))
-            chanmsg(strings.TOOT_PENDING)
-    else:
-        chanmsg(strings.TOOT_USAGE)
+def handle(irc_server, irc_chan, rabbit, command):
+    h = TootHandler(irc_server, irc_chan, rabbit)
+    h.handle(command)
