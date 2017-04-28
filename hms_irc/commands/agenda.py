8a0cd24d96e5a02e0765ce1d7d0ab1171981b5a9
Romain Porte <microjoe@microjoe.org>
2017-04-28 12:47:39 +0200

    Reduced complexity of the agenda command implementation

--- hms_irc/commands/agenda.py
+++ hms_irc/commands/agenda.py
@@ -12,103 +12,105 @@ VALID_ARGUMENTS = VOICED_ARGUMENTS + ['help', 'all']
 class AgendaCommand(IRCHandler):
 
     def handle(self, command):
-
-        # Define some useful closures
-
-        def query(command, args=None):
-            """Closure for sending a query to the spacestatus microservice."""
-            msg =  {
-                'command': command,
-                'source': 'irc'
-            }
-
-            # Add args if provided
-            if args:
-                msg['arguments'] = args
-
-            self.rabbit.publish('agenda.query', msg)
-
-        def help():
-            """Closure for displaying help."""
-            for msg in strings.AGENDA_HELP:
-                self.chanmsg(msg)
-
-        def testregex(regex):
-            result = regex.match(' '.join(command.command_args[1:]))
-
-            if not result:
-                self.chanmsg(strings.UNKNOWN_ARGUMENT)
-                help()
-                return None
-
-            return result
-
+        # If no argument, display agenda event list
         if not command.command_args:
-            query('list')
-        else:
-            # Check argument user provided
-            argument = command.command_args[0]
-            if argument not in VALID_ARGUMENTS:
-                self.chanmsg(strings.UNKNOWN_ARGUMENT)
-                help()
-                return
+            self.query('list')
+            return
 
-            # Check if command needs to be voiced
-            if argument in VOICED_ARGUMENTS and not command.is_voiced:
-                self.chanmsg(strings.MUST_BE_VOICED)
-                return
+        # Check argument user provided is an existing argument
+        argument = command.command_args[0]
+        if argument not in VALID_ARGUMENTS:
+            self.display_unknown_argument()
+            return
 
-            # If help required just display it and do not send query
-            if argument == 'help':
-                help()
-                return
+        # Check if command needs to be voiced
+        if argument in VOICED_ARGUMENTS and not command.is_voiced:
+            self.display_must_be_voiced()
+            return
 
-            if argument == 'all':
-                query('list', {'all': True})
-            if argument == 'add':
-                regex = re.compile(r'(\d{1,2}\/\d{2}\/\d{4}\s\d{1,2}:\d{2})\s"([^"]+)"\s"([^"]+)"(.+)$')
-                result = testregex(regex)
+        # Check all the possible arguments
+        if argument == 'help':
+            self.display_help()
 
-                if result:
-                    date, location, title, desc = result.groups()
-                    query('add', {
-                        'date': date.strip(),
-                        'location': location.strip(),
-                        'title': title.strip(),
-                        'desc': desc.strip()
-                    })
+        elif argument == 'all':
+            self.query('list', {'all': True})
 
-            elif argument == 'add_seance':
-                regex = re.compile(r'(\d{1,2}\/\d{2}\/\d{4}\s\d{1,2}:\d{2})$')
-                result = testregex(regex)
+        elif argument == 'add':
+            regex = re.compile(r'(\d{1,2}\/\d{2}\/\d{4}\s\d{1,2}:\d{2})\s"([^"]+)"\s"([^"]+)"(.+)$')
+            result = self.test_regex(command, regex)
 
-                if result:
-                    date, *_ = result.groups()
-                    query('add_seance', {
-                        'date': date.strip()
-                    })
+            if result:
+                date, location, title, desc = result.groups()
+                self.query('add', {
+                    'date': date.strip(),
+                    'location': location.strip(),
+                    'title': title.strip(),
+                    'desc': desc.strip()
+                })
 
-            elif argument == 'modify':
-                regex = re.compile(r'(\d+)\s(titre|lieu|date|status)\s(.+)$')
-                result = testregex(regex)
+        elif argument == 'add_seance':
+            regex = re.compile(r'(\d{1,2}\/\d{2}\/\d{4}\s\d{1,2}:\d{2})$')
+            result = self.test_regex(command, regex)
 
-                if result:
-                    id, field, new_value = result.groups()
-                    query('modify', {
-                        'id': int(id),
-                        'field': field.strip(),
-                        'new_value': new_value.strip()
-                    })
+            if result:
+                date, *_ = result.groups()
+                self.query('add_seance', {
+                    'date': date.strip()
+                })
 
-            elif argument == 'remove':
-                regex = re.compile(r'(\d+)$')
-                result = testregex(regex)
+        elif argument == 'modify':
+            regex = re.compile(r'(\d+)\s(titre|lieu|date|status)\s(.+)$')
+            result = self.test_regex(command, regex)
 
-                if result:
-                    id, *_ = result.groups()
-                    query('remove', {
-                        'id': int(id),
-                    })
+            if result:
+                id, field, new_value = result.groups()
+                self.query('modify', {
+                    'id': int(id),
+                    'field': field.strip(),
+                    'new_value': new_value.strip()
+                })
+
+        elif argument == 'remove':
+            regex = re.compile(r'(\d+)$')
+            result = self.test_regex(command, regex)
+
+            if result:
+                id, *_ = result.groups()
+                self.query('remove', {
+                    'id': int(id),
+                })
+
+    def display_must_be_voiced(self):
+        self.chanmsg(strings.MUST_BE_VOICED)
+
+    def display_unknown_argument(self):
+        self.chanmsg(strings.UNKNOWN_ARGUMENT)
+        self.display_help()
+
+    def test_regex(self, command, regex):
+        result = regex.match(' '.join(command.command_args[1:]))
+        if not result:
+            self.display_unknown_argument()
+            return None
+        return result
+
+    def query(self, command, args=None):
+        """Closure for sending a query to the spacestatus microservice."""
+        msg = {
+            'command': command,
+            'source': 'irc'
+        }
+
+        # Add args if provided
+        if args:
+            msg['arguments'] = args
+
+        self.rabbit.publish('agenda.query', msg)
+
+    def display_help(self):
+        """Closure for displaying help."""
+        for msg in strings.AGENDA_HELP:
+            self.chanmsg(msg)
 
 
 def handle(irc_server, irc_chan, rabbit, command):
