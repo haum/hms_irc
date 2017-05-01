37099d503404edd194bacebc08c91cdd52f97489
Romain Porte <microjoe@microjoe.org>
2017-05-01 13:50:17 +0200

    Added support for Mastodon mentions

---
+++ hms_irc/receivers/mastodon/mention.py
@@ -0,0 +1,9 @@
+
+
+def handle(irc_server, irc_chan, dct):
+
+    def irc(msg):
+        irc_server.privmsg(irc_chan, msg)
+
+    irc('[Mastodon] ' + dct['user'] + ' « ' + dct['text_content'] + ' » ' +
+        dct['url'])
