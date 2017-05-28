8a8950566afc9359d257bf6f516a29f405be2bb9
Sébastien <sebastien@svallee.fr>
2017-05-28 17:06:45 +0200

    Adding twitter to the party

---
+++ hms_irc/commands/twitter.py
@@ -0,0 +1,37 @@
+import logging
+
+from hms_irc import strings
+from hms_irc.irc import IRCHandler
+
+
+def get_logger():
+    return logging.getLogger(__name__)
+
+
+class TwitterHandler(IRCHandler):
+    def handle(self, command):
+        get_logger().info('Tweet with command {}'.format(command))
+
+        # TODO: Ugly way of testing command arguments...
+        if command.command_args and command.command_args[0]:
+            if command.is_voiced:
+                self.tweet(' '.join(command.command_args))
+                self.chanmsg(strings.TWEET_PENDING)
+        else:
+            self.display_usage()
+
+    def display_usage(self):
+        self.chanmsg(strings.TWEET_USAGE)
+
+    def tweet(self, msg):
+        """Closure for sending a tweet."""
+        self.rabbit.publish(
+            'twitter.query', {
+                'status': msg,
+                'source': 'irc'
+            })
+
+
+def handle(irc_server, irc_chan, rabbit, command):
+    h = TwitterHandler(irc_server, irc_chan, rabbit)
+    h.handle(command)%    
