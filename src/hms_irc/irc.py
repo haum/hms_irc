b159827ce1a038ffd122a10369e1ba8d3d4181fb
Romain Porte <microjoe@microjoe.org>
2017-10-22 10:52:32 +0200

    Added SubcommandIRCHandler class
    This class allows developers to create subcommand-based message
    handlers with ease of use. This feature uses the Python introspection in
    order to discover and call subcommands. Moreover the @voiced decorator
    really simplify declaring that a subcommand must be called with voiced
    rights.

--- src/hms_irc/irc.py
+++ src/hms_irc/irc.py
@@ -1,5 +1,12 @@
 import attr
 
+from hms_irc import strings
+
+
+class NotVoicedError(Exception):
+    """Raised when user call a command but is not voiced"""
+    pass
+
 
 @attr.s
 class IRCCommand:
@@ -11,6 +18,11 @@ class IRCCommand:
 
 @attr.s
 class IRCHandler:
+    """Base IRC handler.
+
+    One must reimplement the ``handle`` method in order to handle commands.
+
+    """
     irc_server = attr.ib()
     irc_chan = attr.ib()
     rabbit = attr.ib()
@@ -21,3 +33,54 @@ class IRCHandler:
 
     def handle(self, command):
         raise NotImplementedError
+
+
+class SubcommandIRCHandler(IRCHandler):
+    """Subcommand-based IRC handler.
+
+    Can handle multiple subcommands easily using introspection.
+    For example the ``!myhandler add`` will call ``handler.cmd_add`` and
+    ``!myhandler delete`` will call ``handler.cmd_delete``.
+
+    """
+
+    def handle(self, command):
+        """Automatic handler, routing to dedicated methods for subcommands."""
+        if not command.command_args:
+            return self.without_subcommand()
+
+        subcommand = command.command_args[0]
+        try:
+            subcommand_handler = getattr(self, "cmd_{}".format(subcommand))
+        except AttributeError:
+            return self.subcommand_not_found(command)
+        else:
+            try:
+                return subcommand_handler(command)
+            except NotVoicedError:
+                self.not_voiced(command)
+
+    def without_subcommand(self):
+        """Called when no subcommand has been provided.
+
+        For example, !myhandler will call this method because no subcommand
+        is provided.
+
+        """
+        raise NotImplementedError
+
+    def subcommand_not_found(self, command):
+        """Called when the provided subcommand was not found."""
+        raise NotImplementedError
+
+    def not_voiced(self, command):
+        self.chanmsg(strings.MUST_BE_VOICED)
+
+
+def voiced(handler):
+    def verify_voiced(instance, command):
+        if not command.is_voiced:
+            raise NotVoicedError
+
+        handler(instance, command)
+    return verify_voiced
